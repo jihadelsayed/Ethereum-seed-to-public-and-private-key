9c29224878722488c7f42d01274d5fecbc9d85af
Jihad Sayed <david.pales1@gmail.com>
2024-07-14 14:11:50 +0200

    Update ethereum-seed-to-public-and-private-key.py

--- ethereum-seed-to-public-and-private-key.py
+++ ethereum-seed-to-public-and-private-key.py
@@ -1,8 +1,9 @@
+import os
 from mnemonic import Mnemonic
 from bip32 import BIP32, HARDENED_INDEX
 from eth_account import Account
 import hashlib
-
+os.system('color')
 def copyRight():
         print(colors.HEADER + """"
             ____   __ __       ____  ____  __ __   ____  ___          ___  _           _____  ____  __ __    ___  ___   
